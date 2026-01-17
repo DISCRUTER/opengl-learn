061ea6e78e4de68a40192ae2c5bd849fa918840e
DISCRUTER <mohitanand10204@gmail.com>
2026-01-17 18:17:19 +0000

    Learned Shader & restruture of project

--- tuts/quad.cpp
+++ tuts/quad.cpp
@@ -12,17 +12,14 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 vertexColor;\n"
     "void main() {\n"
     "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "    vertexColor = aColor;\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
-    "in vec3 vertexColor;\n"
     "out vec4 FragColor;\n"
+    "uniform vec4 ourColor;\n"
     "void main() {\n"
-    "    FragColor = vec4(vertexColor, 1.0);\n"
+    "    FragColor = ourColor;\n"
     "}\0";
 
 int main() {
@@ -46,25 +43,32 @@ int main() {
     }
 
     float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
-    }; 
+         0.5f,  0.5f, 0.0f, // top right
+         0.5f, -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f  // top left
+    };
 
-    // Setup vertex array object (VAO) & vertex buffer object (VBO)
-    unsigned int VAO, VBO;
+    unsigned int indicies[] = {
+        0, 1, 2, // first triangle
+        2, 3, 0  // second triangle
+    };
+
+    // Setup vertex array object (VAO) & vertex buffer object (VBO) & element buffer object (EBO)
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
 
     // Setup vertex shader object
     unsigned int vertexShader;
@@ -119,9 +123,15 @@ int main() {
         // activate the shader program
         glUseProgram(shaderProgram);
 
-        // rendering the triangle
+        // update the uniform color
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        // rendering the quad
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         // check and call events and swap the buffers
--- tuts/two.cpp
+++ tuts/two.cpp
@@ -11,8 +11,10 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main() {\n"
     "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "    vertexColor = vec4(0.5, 0.0, 0.5, 1.0);\n"
     "}\0";
 const char *fragmentShaderSourceOne = "#version 330 core\n"
     "out vec4 FragColor;\n"
